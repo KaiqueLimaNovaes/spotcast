3b2c1d11cebaddae6a34fb7891676da01fe97315
KaqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2021-01-04 18:56:44 -0300

    Att 01

--- screens/player.js
+++ screens/player.js
@@ -6,14 +6,19 @@ import ChevronIcon from '../components/icons/Chevron/index';
 import MoreVertIcon from '../components/icons/MoreVert/index';
 
 const Background = styled.View`
-    flex: 1;
-    background-color: grey;
-    padding-top: 50px;
+  flex: 1;
+  background-color: #A9A9A9;
+  padding-top: 50px;
 `;
 
 const TopBar = styled.View`
   flex-direction: row;
 `;
+
+TopBar.Left = styled.View`flex: 1; padding-left: 16px;`;
+TopBar.Middle = styled.View`flex: 2;`;
+TopBar.Right = styled.View`flex: 1; padding-right: 16px; align-items: flex-end;`;
+
 TopBar.Title = styled.Text`color: white;`;
 TopBar.SubTitle = styled.Text`color: white;`;
 
@@ -21,10 +26,20 @@ export default function PlayerScreen() {
   return (
     <Background>
       <TopBar>
-        <ChevronIcon />
-        <TopBar.Title>Tocando podcast</TopBar.Title>
-        <TopBar.SubTitle>Hipsters ponto tech</TopBar.SubTitle>
-        <MoreVertIcon />
+        
+        <TopBar.Left>
+          <ChevronIcon />
+        </TopBar.Left>
+        
+        <TopBar.Middle>
+          <TopBar.Title>Tocando podcast</TopBar.Title>
+          <TopBar.SubTitle>Hipsters ponto tech</TopBar.SubTitle>
+        </TopBar.Middle>
+        
+        <TopBar.Right>
+          <MoreVertIcon />
+        </TopBar.Right>
+      
       </TopBar>
     </Background>
   );
