3b93650f6f5b737643beaa75a1eb46c26617bd20
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2021-01-05 12:12:29 -0300

    Att 02

--- screens/player.js
+++ screens/player.js
@@ -1,30 +1,42 @@
 import React from 'react';
-import { Text, View } from 'react-native';
 import styled from 'styled-components';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import ChevronIcon from '../components/icons/Chevron/index';
 import MoreVertIcon from '../components/icons/MoreVert/index';
 
+/*
 const Background = styled.View`
   flex: 1;
-  background-color: #A9A9A9;
   padding-top: 50px;
 `;
+*/
+
+const Background = ({ children }) => {
+  return (
+    <LinearGradient
+        colors={['#00008B', '#1C1C1C']}
+        style={{flex: 1, paddingTop: 30,}}
+    >
+      {children}
+    </LinearGradient>
+  );
+};
 
 const TopBar = styled.View`
   flex-direction: row;
 `;
 
 TopBar.Left = styled.View`flex: 1; padding-left: 16px;`;
-TopBar.Middle = styled.View`flex: 2;`;
+TopBar.Middle = styled.View`flex: 2; align-items: center;`;
 TopBar.Right = styled.View`flex: 1; padding-right: 16px; align-items: flex-end;`;
 
-TopBar.Title = styled.Text`color: white;`;
-TopBar.SubTitle = styled.Text`color: white;`;
+TopBar.Title = styled.Text`color: white; text-transform: uppercase;`;
+TopBar.SubTitle = styled.Text`color: white; font-weight: bold;`;
 
 export default function PlayerScreen() {
   return (
-    <Background>
+    <Background>    
       <TopBar>
         
         <TopBar.Left>
